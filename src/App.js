bbc42469f7f79327efab9d84c0659e9be594fe5b
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2023-11-13 21:09:12 +0530

    Initializing (Design Process is Complete)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import React, { useState } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="bg-slate-300 h-[28rem] flex justify-center items-center gap-16 ">
+
+        <button className='px-[1.30rem] pb-[0.50rem] bg-[#EAB308] text-white rounded-2xl text-[2rem] font-bold active:bg-[#ffd452] active:text-black'>-</button>
+        <div className='font-[Orbitron] text-[2rem] text-black'>Output:</div>
+        <button className='px-4 pb-[0.50rem] bg-[#EAB308] text-white rounded-2xl text-[2rem] font-bold active:bg-[#ffd452] active:text-black'>+</button>
+
+      </div>
+      <Footer />
+    </>
   );
 }
 
